b031e4c88be61996461ba84f382cb2dc23c89f3a
Filipe do Couto Castro <filipe.castro.dev@gmail.com>
2021-04-14 23:52:32 -0300

    var | val e declaracao de variaveis

---
+++ src/Aula02-Variaveis.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val name = "Filipe" //imutável
+    val middleName: String = "Couto" //imutável
+    var lastName: String = "Castro"//pode ser modificado
+    lastName = "C@stro"
+
+    println("Full name: $name $middleName $lastName")
+}
