6b86ad882eb5702fc73d480d7c1544a98a1929c7
Filipe do Couto Castro <filipe.castro.dev@gmail.com>
2021-04-14 23:21:13 -0300

    declaracao de funcao e corpo de expresao

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,3 +1,13 @@
 fun main(){
     println("Hello World!")
+    println(max(10,11))
+    println(min(20,7))
 }
+
+//Função com corpo de bloco
+fun max(num1: Int, num2: Int): Int {
+    return if (num1 > num2) num1 else num2
+}
+
+//Função com corpo de expressão. Obs: Indicar o tipo retornado é opcional
+fun min(num1: Int, num2: Int): Int = if (num1 > num2) num2 else num1
