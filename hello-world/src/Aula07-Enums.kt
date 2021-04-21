81786d3b8896bf99cb96c5116b9689a044d28cd9
Filipe do Couto Castro <filipe.castro.dev@gmail.com>
2021-04-20 23:11:06 -0300

    enumeracoes

---
+++ hello-world/src/Aula07-Enums.kt
@@ -0,0 +1,14 @@
+enum class Color(val red: Int, val green: Int, val blue: Int) {
+    RED(255, 0, 0),
+    GREEN(0, 255, 0),
+    BLUE(0, 0, 255),
+    ORANGE(255, 165, 0),
+    YELLOW(255, 255, 0);
+
+    fun rgbToDecimal(): Int = ((red * 256 + green) * 256 + blue)
+}
+
+fun main(){
+    val decimalRed = Color.RED.rgbToDecimal()
+    println(decimalRed)
+}
